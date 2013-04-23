a80d4e5df5c83f16ccf8e553f1c21f60ebe3af13
Piotr Antosik <antek88@gmail.com>
2013-04-23 19:05:53 +0300

    Update mysqldump.php
    PSR standard

--- mysqldump.php
+++ mysqldump.php
@@ -11,8 +11,11 @@
 class MySQLDump
 {
 
-    // This can be set both on counstruct or manually
-    public $host = 'localhost', $user = '', $pass = '', $db = '';
+    // This can be set both on constructor or manually
+    public $host;
+    public $user;
+    public $pass;
+    public $db;
     public $filename = 'dump.sql';
 
     // Usable switch
@@ -37,7 +40,10 @@ class MySQLDump
      */
     public function __construct($db = '', $user = '', $pass = '', $host = 'localhost')
     {
-        $this->db = $db; $this->user = $user; $this->pass = $pass; $this->host = $host;
+        $this->db = $db;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->host = $host;
     }
 
     /**
@@ -49,39 +55,44 @@ class MySQLDump
     public function start($filename = '')
     {
         // Output file can be redefined here
-        if(!empty($filename)) $this->filename = $filename;
+        if (!empty($filename)) {
+            $this->filename = $filename;
+        }
         // We must set a name to continue
-        if(empty($this->filename)) throw new Exception("Output file name is not set", 1);
+        if (empty($this->filename)) {
+            throw new \Exception("Output file name is not set", 1);
+        }
         // Trying to bind a file with block
         if (true === $this->compress) {
             $this->file_handler = gzopen($this->filename, "wb");
-        }
-        else {
+        } else {
             $this->file_handler = fopen($this->filename, "wb");
         }
-        if($this->file_handler === FALSE) throw new Exception("Output file is not writable", 2);
+        if (false === $this->file_handler) {
+            throw new \Exception("Output file is not writable", 2);
+        }
         // Connecting with MySQL
         try {
-            $this->db_handler = new PDO("mysql:dbname={$this->db};host={$this->host}", $this->user, $this->pass);
-        } catch (PDOException $e) {
-            throw new Exception("Connection to MySQL failed with message: ".$e->getMessage(), 3);
+            $this->db_handler = new \PDO("mysql:dbname={$this->db};host={$this->host}", $this->user, $this->pass);
+        } catch (\PDOException $e) {
+            throw new \Exception("Connection to MySQL failed with message: " . $e->getMessage(), 3);
         }
         // Fix for always-unicode output
         $this->db_handler->exec("SET NAMES utf8");
         // https://github.com/clouddueling/mysqldump-php/issues/9
         $this->db_handler->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_NATURAL);
         // Formating dump file
-        $this->write_header();
+        $this->writeHeader();
         // Listing all tables from database
         $this->tables = array();
         foreach ($this->db_handler->query("SHOW TABLES") as $row) {
             array_push($this->tables, current($row));
         }
         // Exporting tables one by one
-        foreach($this->tables as $table) {
+        foreach ($this->tables as $table) {
             $this->write("-- --------------------------------------------------------\n\n");
-            $this->get_table_structure($table);
-            $this->list_values($table);
+            $this->getTableStructure($table);
+            $this->listValues($table);
         }
         // Releasing file
         if (true === $this->compress) {
@@ -97,15 +108,15 @@ class MySQLDump
      * @param string $string  SQL to write to dump file
      * @return bool
      */
-    private function write($string) {
+    private function write($string)
+    {
         if (true === $this->compress) {
-            if(gzwrite($this->file_handler, $string) === FALSE) {
-                throw new Exception("Writting to file failed! Probably, there is no more free space left?", 4);
+            if (false === gzwrite($this->file_handler, $string)) {
+                throw new \Exception("Writting to file failed! Probably, there is no more free space left?", 4);
             }
-        }
-        else {
-            if(fwrite($this->file_handler, $string) === FALSE) {
-                throw new Exception("Writting to file failed! Probably, there is no more free space left?", 4);
+        } else {
+            if (false === fwrite($this->file_handler, $string)) {
+                throw new \Exception("Writting to file failed! Probably, there is no more free space left?", 4);
             }
         }
     }
@@ -115,14 +126,14 @@ class MySQLDump
      *
      * @return null
      */
-    private function write_header()
+    private function writeHeader()
     {
         // Some info about software, source and time
         $this->write("-- mysqldump-php SQL Dump\n");
         $this->write("-- https://github.com/clouddueling/mysqldump-php\n");
         $this->write("--\n");
         $this->write("-- Host: {$this->host}\n");
-        $this->write("-- Generation Time: ".date('r')."\n\n");
+        $this->write("-- Generation Time: " . date('r') . "\n\n");
         $this->write("--\n");
         $this->write("-- Database: `{$this->db}`\n");
         $this->write("--\n\n");
@@ -134,14 +145,14 @@ class MySQLDump
      * @param string $tablename  Name of table to export
      * @return null
      */
-    private function get_table_structure($tablename)
+    private function getTableStructure($tablename)
     {
-        $this->write( "--\n-- Table structure for table `$tablename`\n--\n\n" );
-        if ($this->droptableifexists) {
-            $this->write( "DROP TABLE IF EXISTS `$tablename`;\n\n" );
+        $this->write("--\n-- Table structure for table `$tablename`\n--\n\n");
+        if (true === $this->droptableifexists) {
+            $this->write("DROP TABLE IF EXISTS `$tablename`;\n\n");
         }
         foreach ($this->db_handler->query("SHOW CREATE TABLE `$tablename`") as $row) {
-            $this->write( $row['Create Table'].";\n\n" );
+            $this->write($row['Create Table'] . ";\n\n");
         }
     }
 
@@ -151,18 +162,16 @@ class MySQLDump
      * @param string $tablename  Name of table to export
      * @return null
      */
-    private function list_values($tablename)
+    private function listValues($tablename)
     {
         $this->write("--\n-- Dumping data for table `$tablename`\n--\n\n");
         foreach ($this->db_handler->query("SELECT * FROM `$tablename`", PDO::FETCH_NUM) as $row) {
             $vals = array();
-            foreach($row as $val) {
+            foreach ($row as $val) {
                 $vals[] = is_null($val) ? "NULL" : $this->db_handler->quote($val);
             }
-            $this->write("INSERT INTO `$tablename` VALUES(".implode(", ",$vals).");\n");
+            $this->write("INSERT INTO `$tablename` VALUES(" . implode(", ", $vals) . ");\n");
         }
         $this->write("\n");
     }
 }
-
-?>
