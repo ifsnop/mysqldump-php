88a087c074942f7d54c945dfb7d51d0ac3e60312
Dmitry Krekota <dmitry.krekota@gmail.com>
2017-05-09 15:12:02 +0300

    Fix PDO params

--- tests/test.php
+++ tests/test.php
@@ -94,8 +94,7 @@ $dump = new IMysqldump\Mysqldump();
 $externalDbHandler = @new PDO(
     "mysql:unix_socket=/var/run/mysqld/mysqld.sock;dbname=test011",
     "travis",
-    "travis",
-    ""
+    "travis"
 );
 $dump->setDbHandler($externalDbHandler);
 $dump->start("mysqldump-php_test011b.sql");
