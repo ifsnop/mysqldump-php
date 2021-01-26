992b28ed327c784768f23ed58ea5a803f57366c6
christian <christian.davila@koobin.com>
2021-01-26 13:11:18 +0100

    refactor: changed tabs for space in create_table method for consistency

--- src/Ifsnop/Mysqldump/Mysqldump.php
+++ src/Ifsnop/Mysqldump/Mysqldump.php
@@ -1878,10 +1878,10 @@ class TypeAdapterMysql extends TypeAdapterFactory
             $replace = "";
             $createTable = preg_replace($match, $replace, $createTable);
         }
-        
-		if ($this->dumpSettings['if-not-exists'] ) {
-			$createTable = preg_replace('/^CREATE TABLE/', 'CREATE TABLE IF NOT EXISTS', $createTable);
-        }        
+
+        if ($this->dumpSettings['if-not-exists'] ) {
+                $createTable = preg_replace('/^CREATE TABLE/', 'CREATE TABLE IF NOT EXISTS', $createTable);
+        }
 
         $ret = "/*!40101 SET @saved_cs_client     = @@character_set_client */;".PHP_EOL.
             "/*!40101 SET character_set_client = ".$this->dumpSettings['default-character-set']." */;".PHP_EOL.
